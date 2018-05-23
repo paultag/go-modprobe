7ccf6a1b5271feea8bd2f5a3f3989808f24fc95a
Paul Tagliamonte <tag@pault.ag>
2018-05-23 18:59:01 -0400

    Pass a param to the init'd module

--- load.go
+++ load.go
@@ -8,7 +8,7 @@ import (
 
 // Given a short module name (such as `g_ether`), determine where the kernel
 // module is located, determine any dependencies, and load all required modules.
-func Load(module string) error {
+func Load(module, params string) error {
 	path, err := ResolveName(module)
 	if err != nil {
 		return err
@@ -19,13 +19,17 @@ func Load(module string) error {
 		return err
 	}
 
-	for _, module := range order {
+	paramList := make([]string, len(order))
+	paramList[len(order)-1] = params
+
+	for i, module := range order {
 		fd, err := os.Open(module)
 		if err != nil {
 			return err
 		}
 		/* not doing a defer since we're in a loop */
-		if err := Init(fd, ""); err != nil && err != unix.EEXIST {
+		param := paramList[i]
+		if err := Init(fd, param); err != nil && err != unix.EEXIST {
 			fd.Close()
 			return err
 		}
